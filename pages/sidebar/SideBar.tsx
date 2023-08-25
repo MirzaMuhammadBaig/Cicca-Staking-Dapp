76188093068a75d22aec4dd9ba4704dfac635851
MirzaMuhammadBaig <mirzamuhammadbaig328@gmail.com>
2023-08-25 16:03:13 +0500

    sidebar-almost-completed

--- pages/sidebar/SideBar.tsx
+++ pages/sidebar/SideBar.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Link from 'next/link';
+
+import { TiFlashOutline } from 'react-icons/ti'
+import { FaArrowsAltV } from 'react-icons/fa'
+import { RiTokenSwapLine } from 'react-icons/ri'
+
 
 const SideBar = () => {
+
+   const [activeItem, setActiveItem] = useState<string | null>(null);
+
+   const handleItemClick = (itemName: string) => {
+      setActiveItem(itemName);
+   };
+
    return (
       <div>
-
          <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
             <span className="sr-only">Open sidebar</span>
             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-               <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+               <path clipRule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
             </svg>
          </button>
 
@@ -15,29 +27,23 @@ const SideBar = () => {
             <div className="h-full px-3 py-4 overflow-y-auto ">
                <ul className="space-y-2 font-small">
 
-                  <li className='p-1 bg-slate-100 rounded-lg'>
-                     <a href="#" className="flex items-center p-2 rounded-lg group">
-                        <svg className="flex-shrink-0 w-5 h-5 text-black transition duration-75 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                           <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
-                        </svg>
-                        <span className="flex-1 ml-3 whitespace-nowrap">Products</span>
-                     </a>
+                  <li className={`p-1 ${activeItem === 'staking' ? 'bg-red-700' : 'bg-slate-100'} rounded-lg`}>
+                     <p className="flex items-center p-2 rounded-lg group" onClick={() => handleItemClick('staking')}>
+                        <TiFlashOutline size="1.4em" color={activeItem === 'staking' ? 'white' : 'black'} />
+                        <Link className={`flex-1 ml-3 whitespace-nowrap ${activeItem === 'staking' ? 'text-white' : ''}`} href="/staking">Staking</Link>
+                     </p>
                   </li>
-                  <li className='p-1 bg-slate-100 rounded-lg'>
-                     <a href="#" className="flex items-center p-2 rounded-lg group">
-                        <svg className="flex-shrink-0 w-5 h-5 text-black transition duration-75 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
-                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3" />
-                        </svg>
-                        <span className="flex-1 ml-3 whitespace-nowrap">Sign In</span>
-                     </a>
+                  <li className={`p-1 ${activeItem === 'bridge' ? 'bg-red-700' : 'bg-slate-100'} rounded-lg`}>
+                     <p className="flex items-center p-2 rounded-lg group" onClick={() => handleItemClick('bridge')}>
+                        <FaArrowsAltV size="1.4em" color={activeItem === 'bridge' ? 'white' : 'black'} />
+                        <Link className={`flex-1 ml-3 whitespace-nowrap ${activeItem === 'bridge' ? 'text-white' : ''}`} href="/bridge">Bridge</Link>
+                     </p>
                   </li>
-                  <li className='p-1 bg-slate-100 rounded-lg'>
-                     <a href="#" className="flex items-center p-2 rounded-lg group">
-                        <svg className="flex-shrink-0 w-5 h-5 text-black transition duration-75 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
-                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3" />
-                        </svg>
-                        <span className="flex-1 ml-3 whitespace-nowrap">Sign In</span>
-                     </a>
+                  <li className={`p-1 ${activeItem === 'swap' ? 'bg-red-700' : 'bg-slate-100'} rounded-lg`}>
+                     <p className="flex items-center p-2 rounded-lg group" onClick={() => handleItemClick('swap')}>
+                        <RiTokenSwapLine size="1.4em" color={activeItem === 'swap' ? 'white' : 'black'} />
+                        <Link className={`flex-1 ml-3 whitespace-nowrap ${activeItem === 'swap' ? 'text-white' : ''}`} href="/swap">Swap</Link>
+                     </p>
                   </li>
                </ul>
             </div>
